levackt/solidity

Use "none" and "full" optimizer settings in the optimizer tests.

test/ExecutionFramework.cpp
@@ -57,7 +57,7 @@ ExecutionFramework::ExecutionFramework():
 ExecutionFramework::ExecutionFramework(string const& _ipcPath):
 	m_rpc(RPCSession::instance(_ipcPath)),
 	m_evmVersion(dev::test::Options::get().evmVersion()),
-	m_optimize(dev::test::Options::get().optimize),
+	m_optimiserSettings(dev::test::Options::get().optimize ? solidity::OptimiserSettings::minimal() : solidity::OptimiserSettings::enabled()),
 	m_showMessages(dev::test::Options::get().showMessages),
 	m_sender(m_rpc.account(0))
 {

SmartDeveloper741/learning-solidity

test/ExecutionFramework.cpp
@@ -57,7 +57,7 @@ ExecutionFramework::ExecutionFramework():
 ExecutionFramework::ExecutionFramework(string const& _ipcPath, langutil::EVMVersion _evmVersion):
 	m_rpc(RPCSession::instance(_ipcPath)),
 	m_evmVersion(_evmVersion),
-	m_optimize(dev::test::Options::get().optimize),
+	m_optimiserSettings(dev::test::Options::get().optimize ? solidity::OptimiserSettings::enabled() : solidity::OptimiserSettings::minimal()),
 	m_showMessages(dev::test::Options::get().showMessages),
 	m_sender(m_rpc.account(0))
 {