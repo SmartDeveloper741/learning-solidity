salamani/solidity

Use updated swarm hash.

libsolidity/interface/CompilerStack.cpp
@@ -766,7 +766,7 @@ h256 const& CompilerStack::Source::keccak256() const
 h256 const& CompilerStack::Source::swarmHash() const
 {
 	if (swarmHashCached == h256{})
-		swarmHashCached = dev::swarmHash(scanner->source());
+		swarmHashCached = dev::bzzHash(scanner->source());
 	return swarmHashCached;
 }
 
@@ -1037,7 +1037,7 @@ string CompilerStack::createMetadata(Contract const& _contract) const
 		else
 		{
 			meta["sources"][s.first]["urls"] = Json::arrayValue;
-			meta["sources"][s.first]["urls"].append("bzzr://" + toHex(s.second.swarmHash().asBytes()));
+			meta["sources"][s.first]["urls"].append("bzz-raw://" + toHex(s.second.swarmHash().asBytes()));
 			meta["sources"][s.first]["urls"].append(s.second.ipfsUrl());
 		}
 	}
@@ -1184,7 +1184,7 @@ private:
 bytes CompilerStack::createCBORMetadata(string const& _metadata, bool _experimentalMode)
 {
 	MetadataCBOREncoder encoder;
-	encoder.pushBytes("bzzr0", dev::swarmHash(_metadata).asBytes());
+	encoder.pushBytes("bzzr1", dev::bzzHash(_metadata).asBytes());
 	if (_experimentalMode)
 		encoder.pushBool("experimental", true);
 	if (m_release)

test/libsolidity/GasCosts.cpp
@@ -40,18 +40,18 @@ namespace test
 #define CHECK_DEPLOY_GAS(_gasNoOpt, _gasOpt) \
 	do \
 	{ \
-		u256 bzzr0Cost = GasMeter::dataGas(dev::swarmHash(m_compiler.metadata(m_compiler.lastContractName())).asBytes(), true); \
+		u256 bzzr1Cost = GasMeter::dataGas(dev::bzzHash(m_compiler.metadata(m_compiler.lastContractName())).asBytes(), true); \
 		u256 gasOpt{_gasOpt}; \
 		u256 gasNoOpt{_gasNoOpt}; \
 		u256 gas = m_optimiserSettings == OptimiserSettings::minimal() ? gasNoOpt : gasOpt; \
 		BOOST_CHECK_MESSAGE( \
-			m_gasUsed >= bzzr0Cost, \
+			m_gasUsed >= bzzr1Cost, \
 			"Gas used: " + \
 			m_gasUsed.str() + \
-			" is less than the data cost for the bzzr0 hash: " + \
-			u256(bzzr0Cost).str() \
+			" is less than the data cost for the bzzr1 hash: " + \
+			u256(bzzr1Cost).str() \
 		); \
-		u256 gasUsed = m_gasUsed - bzzr0Cost; \
+		u256 gasUsed = m_gasUsed - bzzr1Cost; \
 		BOOST_CHECK_MESSAGE( \
 			gas == gasUsed, \
 			"Gas used: " + \

test/libsolidity/Metadata.cpp
@@ -70,7 +70,7 @@ BOOST_AUTO_TEST_CASE(metadata_stamp)
 		bytes const& bytecode = compilerStack.runtimeObject("test").bytecode;
 		std::string const& metadata = compilerStack.metadata("test");
 		BOOST_CHECK(dev::test::isValidMetadata(metadata));
-		bytes hash = dev::swarmHash(metadata).asBytes();
+		bytes hash = dev::bzzHash(metadata).asBytes();
 		BOOST_REQUIRE(hash.size() == 32);
 		auto const cborMetadata = requireParsedCBORMetadata(bytecode);
 		BOOST_CHECK(cborMetadata.size() == 2);
@@ -79,8 +79,8 @@ BOOST_AUTO_TEST_CASE(metadata_stamp)
 			BOOST_CHECK(cborMetadata.at("solc") == toHex(VersionCompactBytes));
 		else
 			BOOST_CHECK(cborMetadata.at("solc") == VersionStringStrict);
-		BOOST_CHECK(cborMetadata.count("bzzr0") == 1);
-		BOOST_CHECK(cborMetadata.at("bzzr0") == toHex(hash));
+		BOOST_CHECK(cborMetadata.count("bzzr1") == 1);
+		BOOST_CHECK(cborMetadata.at("bzzr1") == toHex(hash));
 	}
 }
 
@@ -105,7 +105,7 @@ BOOST_AUTO_TEST_CASE(metadata_stamp_experimental)
 		bytes const& bytecode = compilerStack.runtimeObject("test").bytecode;
 		std::string const& metadata = compilerStack.metadata("test");
 		BOOST_CHECK(dev::test::isValidMetadata(metadata));
-		bytes hash = dev::swarmHash(metadata).asBytes();
+		bytes hash = dev::bzzHash(metadata).asBytes();
 		BOOST_REQUIRE(hash.size() == 32);
 		auto const cborMetadata = requireParsedCBORMetadata(bytecode);
 		BOOST_CHECK(cborMetadata.size() == 3);
@@ -114,8 +114,8 @@ BOOST_AUTO_TEST_CASE(metadata_stamp_experimental)
 			BOOST_CHECK(cborMetadata.at("solc") == toHex(VersionCompactBytes));
 		else
 			BOOST_CHECK(cborMetadata.at("solc") == VersionStringStrict);
-		BOOST_CHECK(cborMetadata.count("bzzr0") == 1);
-		BOOST_CHECK(cborMetadata.at("bzzr0") == toHex(hash));
+		BOOST_CHECK(cborMetadata.count("bzzr1") == 1);
+		BOOST_CHECK(cborMetadata.at("bzzr1") == toHex(hash));
 		BOOST_CHECK(cborMetadata.count("experimental") == 1);
 		BOOST_CHECK(cborMetadata.at("experimental") == "true");
 	}

SmartDeveloper741/learning-solidity

libsolidity/interface/CompilerStack.cpp
@@ -784,7 +784,7 @@ h256 const& CompilerStack::Source::keccak256() const
 h256 const& CompilerStack::Source::swarmHash() const
 {
 	if (swarmHashCached == h256{})
-		swarmHashCached = dev::bzzr0Hash(scanner->source());
+		swarmHashCached = dev::bzzr1Hash(scanner->source());
 	return swarmHashCached;
 }
 
@@ -1085,7 +1085,7 @@ string CompilerStack::createMetadata(Contract const& _contract) const
 		else
 		{
 			meta["sources"][s.first]["urls"] = Json::arrayValue;
-			meta["sources"][s.first]["urls"].append("bzzr://" + toHex(s.second.swarmHash().asBytes()));
+			meta["sources"][s.first]["urls"].append("bzz-raw://" + toHex(s.second.swarmHash().asBytes()));
 			meta["sources"][s.first]["urls"].append(s.second.ipfsUrl());
 		}
 	}
@@ -1232,7 +1232,7 @@ private:
 bytes CompilerStack::createCBORMetadata(string const& _metadata, bool _experimentalMode)
 {
 	MetadataCBOREncoder encoder;
-	encoder.pushBytes("bzzr0", dev::bzzr0Hash(_metadata).asBytes());
+	encoder.pushBytes("bzzr1", dev::bzzr1Hash(_metadata).asBytes());
 	if (_experimentalMode)
 		encoder.pushBool("experimental", true);
 	if (m_release)

test/libsolidity/GasCosts.cpp
@@ -40,18 +40,18 @@ namespace test
 #define CHECK_DEPLOY_GAS(_gasNoOpt, _gasOpt) \
 	do \
 	{ \
-		u256 bzzr0Cost = GasMeter::dataGas(dev::bzzr0Hash(m_compiler.metadata(m_compiler.lastContractName())).asBytes(), true); \
+		u256 bzzr1Cost = GasMeter::dataGas(dev::bzzr1Hash(m_compiler.metadata(m_compiler.lastContractName())).asBytes(), true); \
 		u256 gasOpt{_gasOpt}; \
 		u256 gasNoOpt{_gasNoOpt}; \
 		u256 gas = m_optimiserSettings == OptimiserSettings::minimal() ? gasNoOpt : gasOpt; \
 		BOOST_CHECK_MESSAGE( \
-			m_gasUsed >= bzzr0Cost, \
+			m_gasUsed >= bzzr1Cost, \
 			"Gas used: " + \
 			m_gasUsed.str() + \
-			" is less than the data cost for the bzzr0 hash: " + \
-			u256(bzzr0Cost).str() \
+			" is less than the data cost for the bzzr1 hash: " + \
+			u256(bzzr1Cost).str() \
 		); \
-		u256 gasUsed = m_gasUsed - bzzr0Cost; \
+		u256 gasUsed = m_gasUsed - bzzr1Cost; \
 		BOOST_CHECK_MESSAGE( \
 			gas == gasUsed, \
 			"Gas used: " + \

test/libsolidity/Metadata.cpp
@@ -70,7 +70,7 @@ BOOST_AUTO_TEST_CASE(metadata_stamp)
 		bytes const& bytecode = compilerStack.runtimeObject("test").bytecode;
 		std::string const& metadata = compilerStack.metadata("test");
 		BOOST_CHECK(dev::test::isValidMetadata(metadata));
-		bytes hash = dev::bzzr0Hash(metadata).asBytes();
+		bytes hash = dev::bzzr1Hash(metadata).asBytes();
 		BOOST_REQUIRE(hash.size() == 32);
 		auto const cborMetadata = requireParsedCBORMetadata(bytecode);
 		BOOST_CHECK(cborMetadata.size() == 2);
@@ -79,8 +79,8 @@ BOOST_AUTO_TEST_CASE(metadata_stamp)
 			BOOST_CHECK(cborMetadata.at("solc") == toHex(VersionCompactBytes));
 		else
 			BOOST_CHECK(cborMetadata.at("solc") == VersionStringStrict);
-		BOOST_CHECK(cborMetadata.count("bzzr0") == 1);
-		BOOST_CHECK(cborMetadata.at("bzzr0") == toHex(hash));
+		BOOST_CHECK(cborMetadata.count("bzzr1") == 1);
+		BOOST_CHECK(cborMetadata.at("bzzr1") == toHex(hash));
 	}
 }
 
@@ -105,7 +105,7 @@ BOOST_AUTO_TEST_CASE(metadata_stamp_experimental)
 		bytes const& bytecode = compilerStack.runtimeObject("test").bytecode;
 		std::string const& metadata = compilerStack.metadata("test");
 		BOOST_CHECK(dev::test::isValidMetadata(metadata));
-		bytes hash = dev::bzzr0Hash(metadata).asBytes();
+		bytes hash = dev::bzzr1Hash(metadata).asBytes();
 		BOOST_REQUIRE(hash.size() == 32);
 		auto const cborMetadata = requireParsedCBORMetadata(bytecode);
 		BOOST_CHECK(cborMetadata.size() == 3);
@@ -114,8 +114,8 @@ BOOST_AUTO_TEST_CASE(metadata_stamp_experimental)
 			BOOST_CHECK(cborMetadata.at("solc") == toHex(VersionCompactBytes));
 		else
 			BOOST_CHECK(cborMetadata.at("solc") == VersionStringStrict);
-		BOOST_CHECK(cborMetadata.count("bzzr0") == 1);
-		BOOST_CHECK(cborMetadata.at("bzzr0") == toHex(hash));
+		BOOST_CHECK(cborMetadata.count("bzzr1") == 1);
+		BOOST_CHECK(cborMetadata.at("bzzr1") == toHex(hash));
 		BOOST_CHECK(cborMetadata.count("experimental") == 1);
 		BOOST_CHECK(cborMetadata.at("experimental") == "true");
 	}