ethereum/solidity

Allow path syntax for super constructor calls

libsolidity/analysis/ImmutableValidator.h
@@ -53,6 +53,7 @@ private:
 	bool visit(MemberAccess const& _memberAccess);
 	bool visit(IfStatement const& _ifStatement);
 	bool visit(WhileStatement const& _whileStatement);
+	void endVisit(UserDefinedTypeName const& _userDefinedTypeName);
 	void endVisit(Identifier const& _identifier);
 	void endVisit(Return const& _return);
 

SmartDeveloper741/learning-solidity

libsolidity/analysis/ImmutableValidator.h
@@ -53,6 +53,7 @@ private:
 	bool visit(MemberAccess const& _memberAccess);
 	bool visit(IfStatement const& _ifStatement);
 	bool visit(WhileStatement const& _whileStatement);
+	void endVisit(IdentifierPath const& _identifierPath);
 	void endVisit(Identifier const& _identifier);
 	void endVisit(Return const& _return);
 