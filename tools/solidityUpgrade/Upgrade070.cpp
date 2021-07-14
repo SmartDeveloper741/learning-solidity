yjkellyjoo/solidity

Remove CharStream from SourceLocation.

tools/solidityUpgrade/Upgrade070.cpp
@@ -29,14 +29,14 @@ void DotSyntax::endVisit(FunctionCall const& _functionCall)
 			m_changes.emplace_back(
 				UpgradeChange::Level::Safe,
 				_functionCall.location(),
-				SourceTransform::valueUpdate(_functionCall.location())
+				SourceTransform{m_scanner}.valueUpdate(_functionCall.location())
 			);
 
 		if (funcType->gasSet())
 			m_changes.emplace_back(
 				UpgradeChange::Level::Safe,
 				_functionCall.location(),
-				SourceTransform::gasUpdate(_functionCall.location())
+				SourceTransform{m_scanner}.gasUpdate(_functionCall.location())
 			);
 	}
 }
@@ -55,7 +55,7 @@ void NowKeyword::endVisit(Identifier const& _identifier)
 			m_changes.emplace_back(
 				UpgradeChange::Level::Safe,
 				_identifier.location(),
-				SourceTransform::nowUpdate(_identifier.location())
+				SourceTransform{m_scanner}.nowUpdate(_identifier.location())
 			);
 		}
 }
@@ -72,7 +72,7 @@ void ConstructorVisibility::endVisit(ContractDefinition const& _contract)
 				m_changes.emplace_back(
 					UpgradeChange::Level::Safe,
 					_contract.location(),
-					SourceTransform::insertBeforeKeyword(_contract.location(), "contract", "abstract")
+					SourceTransform{m_scanner}.insertBeforeKeyword(_contract.location(), "contract", "abstract")
 				);
 
 	for (FunctionDefinition const* function: _contract.definedFunctions())
@@ -80,6 +80,6 @@ void ConstructorVisibility::endVisit(ContractDefinition const& _contract)
 			m_changes.emplace_back(
 				UpgradeChange::Level::Safe,
 				function->location(),
-				SourceTransform::removeVisibility(function->location())
+				SourceTransform{m_scanner}.removeVisibility(function->location())
 			);
 }

tools/solidityUpgrade/UpgradeChange.h
@@ -19,6 +19,7 @@
 #include <libsolutil/AnsiColorized.h>
 
 #include <liblangutil/SourceLocation.h>
+#include <liblangutil/ScannerBySourceName.h>
 
 #include <algorithm>
 #include <utility>
@@ -47,32 +48,27 @@ public:
 		Level _level,
 		langutil::SourceLocation _location,
 		std::string _patch
-	)
-	:
+	):
 		m_location(_location),
-		m_source(_location.source->source()),
 		m_patch(std::move(_patch)),
-		m_level(_level) {}
+		m_level(_level)
+	{}
 
 	~UpgradeChange() {}
 
-	langutil::SourceLocation const& location() { return m_location; }
-	std::string source() const { return m_source; }
-	std::string patch() { return m_patch; }
+	langutil::SourceLocation const& location() const { return m_location; }
+	std::string patch() const { return m_patch; }
 	Level level() const { return m_level; }
 
-	/// Does the actual replacement of code under at current source location.
-	/// The change is applied on the upgrade-specific copy of source code.
-	/// The altered code is then requested by the upgrade routine later on.
-	void apply();
-	/// Does a pretty-print of this upgrade change. It uses a source formatter
-	/// provided by the compiler in order to print affected code. Since the patch
+	/// Performs the actual replacement on the provided original source code
+	/// and returns the modified source code.
+	std::string apply(std::string _source) const;
+	/// Does a pretty-print of this upgrade change. Since the patch
 	/// can contain a lot of code lines, it can be shortened, which is signaled
 	/// by setting the flag.
-	void log(bool const _shorten = true) const;
+	void log(langutil::ScannerBySourceName const& _scanner, bool const _shorten = true) const;
 private:
 	langutil::SourceLocation m_location;
-	std::string m_source;
 	std::string m_patch;
 	Level m_level;
 

SmartDeveloper741/learning-solidity

tools/solidityUpgrade/Upgrade070.cpp
@@ -29,14 +29,14 @@ void DotSyntax::endVisit(FunctionCall const& _functionCall)
 			m_changes.emplace_back(
 				UpgradeChange::Level::Safe,
 				_functionCall.location(),
-				SourceTransform::valueUpdate(_functionCall.location())
+				SourceTransform{m_charStreamProvider}.valueUpdate(_functionCall.location())
 			);
 
 		if (funcType->gasSet())
 			m_changes.emplace_back(
 				UpgradeChange::Level::Safe,
 				_functionCall.location(),
-				SourceTransform::gasUpdate(_functionCall.location())
+				SourceTransform{m_charStreamProvider}.gasUpdate(_functionCall.location())
 			);
 	}
 }
@@ -55,7 +55,7 @@ void NowKeyword::endVisit(Identifier const& _identifier)
 			m_changes.emplace_back(
 				UpgradeChange::Level::Safe,
 				_identifier.location(),
-				SourceTransform::nowUpdate(_identifier.location())
+				SourceTransform{m_charStreamProvider}.nowUpdate(_identifier.location())
 			);
 		}
 }
@@ -72,7 +72,7 @@ void ConstructorVisibility::endVisit(ContractDefinition const& _contract)
 				m_changes.emplace_back(
 					UpgradeChange::Level::Safe,
 					_contract.location(),
-					SourceTransform::insertBeforeKeyword(_contract.location(), "contract", "abstract")
+					SourceTransform{m_charStreamProvider}.insertBeforeKeyword(_contract.location(), "contract", "abstract")
 				);
 
 	for (FunctionDefinition const* function: _contract.definedFunctions())
@@ -80,6 +80,6 @@ void ConstructorVisibility::endVisit(ContractDefinition const& _contract)
 			m_changes.emplace_back(
 				UpgradeChange::Level::Safe,
 				function->location(),
-				SourceTransform::removeVisibility(function->location())
+				SourceTransform{m_charStreamProvider}.removeVisibility(function->location())
 			);
 }

tools/solidityUpgrade/UpgradeChange.h
@@ -19,6 +19,7 @@
 #include <libsolutil/AnsiColorized.h>
 
 #include <liblangutil/SourceLocation.h>
+#include <liblangutil/CharStreamProvider.h>
 
 #include <algorithm>
 #include <utility>
@@ -47,32 +48,27 @@ public:
 		Level _level,
 		langutil::SourceLocation _location,
 		std::string _patch
-	)
-	:
+	):
 		m_location(_location),
-		m_source(_location.source->source()),
 		m_patch(std::move(_patch)),
-		m_level(_level) {}
+		m_level(_level)
+	{}
 
 	~UpgradeChange() {}
 
-	langutil::SourceLocation const& location() { return m_location; }
-	std::string source() const { return m_source; }
-	std::string patch() { return m_patch; }
+	langutil::SourceLocation const& location() const { return m_location; }
+	std::string patch() const { return m_patch; }
 	Level level() const { return m_level; }
 
-	/// Does the actual replacement of code under at current source location.
-	/// The change is applied on the upgrade-specific copy of source code.
-	/// The altered code is then requested by the upgrade routine later on.
-	void apply();
-	/// Does a pretty-print of this upgrade change. It uses a source formatter
-	/// provided by the compiler in order to print affected code. Since the patch
+	/// Performs the actual replacement on the provided original source code
+	/// and returns the modified source code.
+	std::string apply(std::string _source) const;
+	/// Does a pretty-print of this upgrade change. Since the patch
 	/// can contain a lot of code lines, it can be shortened, which is signaled
 	/// by setting the flag.
-	void log(bool const _shorten = true) const;
+	void log(langutil::CharStreamProvider const& _charStreamProvider, bool const _shorten = true) const;
 private:
 	langutil::SourceLocation m_location;
-	std::string m_source;
 	std::string m_patch;
 	Level m_level;
 