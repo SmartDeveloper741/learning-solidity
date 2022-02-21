w84us/solidity

Extract external function part into its own function.

libsolidity/codegen/ir/Common.cpp
@@ -41,6 +41,15 @@ YulArity YulArity::fromType(FunctionType const& _functionType)
 	};
 }
 
+string IRNames::externalFunctionPart(Declaration const& _functionOrVarDecl)
+{
+	if (auto const* function = dynamic_cast<FunctionDefinition const*>(&_functionOrVarDecl))
+		solAssert(!function->isConstructor());
+
+	return "external_fun_" + _functionOrVarDecl.name() + "_" + to_string(_functionOrVarDecl.id());
+}
+
+
 string IRNames::function(FunctionDefinition const& _function)
 {
 	if (_function.isConstructor())

libsolidity/codegen/ir/Common.h
@@ -49,6 +49,7 @@ struct YulArity
 
 struct IRNames
 {
+	static std::string externalFunctionPart(Declaration const& _functionOrVardecl);
 	static std::string function(FunctionDefinition const& _function);
 	static std::string function(VariableDeclaration const& _varDecl);
 	static std::string modifierInvocation(ModifierInvocation const& _modifierInvocation);

SmartDeveloper741/learning-solidity

libsolidity/codegen/ir/Common.cpp
@@ -41,6 +41,14 @@ YulArity YulArity::fromType(FunctionType const& _functionType)
 	};
 }
 
+string IRNames::externalFunctionABIWrapper(Declaration const& _functionOrVarDecl)
+{
+	if (auto const* function = dynamic_cast<FunctionDefinition const*>(&_functionOrVarDecl))
+		solAssert(!function->isConstructor());
+
+	return "external_fun_" + _functionOrVarDecl.name() + "_" + to_string(_functionOrVarDecl.id());
+}
+
 string IRNames::function(FunctionDefinition const& _function)
 {
 	if (_function.isConstructor())

libsolidity/codegen/ir/Common.h
@@ -49,6 +49,7 @@ struct YulArity
 
 struct IRNames
 {
+	static std::string externalFunctionABIWrapper(Declaration const& _functionOrVardecl);
 	static std::string function(FunctionDefinition const& _function);
 	static std::string function(VariableDeclaration const& _varDecl);
 	static std::string modifierInvocation(ModifierInvocation const& _modifierInvocation);